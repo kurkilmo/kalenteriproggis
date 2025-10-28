1dd2cbe410383b51fe70ab2b7ebc0f0960064a47
Vilma Vartiainen <vilma.vartiainen@icloud.com>
2025-10-28 12:34:13 +0200

    Menuvalikko

---
+++ frontend/app/(drawer)/_layout.tsx
@@ -0,0 +1,21 @@
+import { Drawer } from 'expo-router/drawer';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+export default function DrawerLayout() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer
+        screenOptions={{
+            drawerActiveTintColor : 'lightpink'
+        }}>
+        <Drawer.Screen
+            name="(tabs)"
+            options={{
+            drawerLabel: 'Etusivu',
+            title: 'Menu'
+        }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
+  );
+}
