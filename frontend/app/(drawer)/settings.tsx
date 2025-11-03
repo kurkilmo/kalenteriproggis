be8e731f0dacfdcdb3f991fad930f95a4709a7dc
migzels <mikkooin23@gmail.com>
2025-11-03 20:00:46 +0200

    Päivitetty settings sivua

--- frontend/app/(drawer)/settings.tsx
+++ frontend/app/(drawer)/settings.tsx
@@ -1,36 +1,93 @@
-import { useState } from 'react'
-import { View, Text, StyleSheet, Switch, Button } from 'react-native'
+import { createContext, useContext, useState } from 'react'
+import { View, Text, StyleSheet, Switch, Button, Modal, TouchableOpacity, Platform } from 'react-native'
+import { ThemedView } from '@/components/themed-view';
+/** Otetaan modaaleille tyylit toistaiseksi EventView tyyleistä */
+import evStyles from '@/styles/eventViewStyle';
+import {Picker} from '@react-native-picker/picker';
 
-const SettingsElement: React.FC<React.PropsWithChildren> = ({children}) => {
-    return (<View style={styles.settingsView}>{children}</View>)
+/** View */
+const SettingsView: React.FC<React.PropsWithChildren> = ({children}) => {
+    return (<ThemedView style={styles.settingsView}>{children}</ThemedView>)
 }
+/** Text */
 const SettingsText: React.FC<React.PropsWithChildren> = ({children}) => {
     return (<Text style={styles.baseText}>{children}</Text>)
 }
 
+export interface Settings {
+    theme: string
+    language: string
+    timezone: string
+}
+
+export const initialSettings: Settings = {
+    theme: 'default',
+    language: 'fi',
+    timezone: 'EET'
+}
+
+export const SettingsContext = createContext<Settings>(initialSettings);
+
 export default function Settings() {
-    const [isLightModeEnabled, setLightModeEnabled] = useState(true)
-    return <View>
+    const settings = useContext(SettingsContext)
+    const [selectedTheme, setSelectedTheme] = useState(true)
+    const [isSelectTimezoneModalVisible, setSelectTimezoneModalVisible] = useState(false)
+    
+
+    return (<ThemedView>
         <Text style={styles.titleText}>Asetukset</Text>
-        <SettingsElement>
-            <Text style={styles.baseText}>Kieli</Text>
-            <Button title="Valitse"/>
-        </SettingsElement>
-        <SettingsElement>
-            <Text style={styles.baseText}>Tumma teema</Text>
-            <Switch
-                onValueChange={() => setLightModeEnabled(!isLightModeEnabled)}
-                value={isLightModeEnabled}
-            />
-        </SettingsElement>
-        <SettingsElement>
+
+        {/** Asetuksia.*/}
+        <SettingsView>
+            <SettingsText>Kieli</SettingsText>
+            <Picker
+                selectedValue={selectedTheme}
+                onValueChange={(itemValue, itemIndex) =>
+                    setSelectedTheme(itemValue)
+                }
+                style={styles.pickerStyle}
+                >
+                <Picker.Item label="Suomi" value="fin" />
+                <Picker.Item label="English" value="eng" />
+            </Picker>
+        </SettingsView>
+        <SettingsView>
+            <SettingsText>Valitse teema</SettingsText>
+            <Picker
+                selectedValue={selectedTheme}
+                onValueChange={(itemValue, itemIndex) =>
+                    setSelectedTheme(itemValue)
+                }
+                style={styles.pickerStyle}
+                >
+                <Picker.Item label="Oletus" value="default" />
+                <Picker.Item label="Vaalea" value="light" />
+                <Picker.Item label="Tumma" value="dark" />
+            </Picker>
+        </SettingsView>
+        <SettingsView>
             <SettingsText>Aikavyöhyke</SettingsText>
-            <Button title="Valitse"/>
-        </SettingsElement>
-        <SettingsElement>
+            <Button title="Valitse" onPress={() => setSelectTimezoneModalVisible(true)}/>
+        </SettingsView>
+        <SettingsView>
             <Button color="#f00" title="Poista käyttäjä"/>
-        </SettingsElement>
-    </View>
+        </SettingsView>
+
+        {/** Aikavyöhyke modaali */}
+        <Modal visible={isSelectTimezoneModalVisible} animationType="fade" transparent={true} onRequestClose={() => setSelectTimezoneModalVisible(false)}>
+            <View style={evStyles.modalBackground}>
+                <View style={evStyles.modalContent}>
+                <Text style={evStyles.modalTitle}>Valitse aikavyöhyke</Text>
+                <Text style={evStyles.modalText}>...aikavyöhykkeitä...</Text>
+                <TouchableOpacity style={evStyles.button} onPress={() => setSelectTimezoneModalVisible(false)}>
+                    <Text style={evStyles.buttonText}>Sulje</Text>
+                </TouchableOpacity>
+                </View>
+            </View>
+        </Modal>
+    </ThemedView>
+    
+    )
 }
 
 
@@ -52,6 +109,21 @@ const styles = StyleSheet.create({
         marginLeft: 25,
         marginRight: 25,
         marginTop: 20,
-        
+        minWidth: 50,
+        minHeight: 50,
+        flex: 0,
+    },
+    /** Eri tyylit alustan mukaan */
+    pickerStyle: {
+        ...Platform.select({
+            android: {
+                height: 75,
+                width: 150,
+                backgroundColor: '#ffffff',
+            },
+            default: {
+
+            }
+        })        
     }
 })
