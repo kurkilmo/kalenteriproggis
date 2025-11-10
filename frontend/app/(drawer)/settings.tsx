de2f221ecdadccfae423b8c4a8e97243faba8c3b
migzels <mikkooin23@gmail.com>
2025-11-10 16:16:16 +0200

    Edistetty lokalisaatiota sekä tehty alustava aikavyöhykkeiden säätö

--- frontend/app/(drawer)/settings.tsx
+++ frontend/app/(drawer)/settings.tsx
@@ -5,13 +5,14 @@ import { ThemedView } from '@/components/themed-view';
 import evStyles from '@/styles/eventViewStyle';
 import {Picker} from '@react-native-picker/picker';
 
-import { getLocales } from 'expo-localization'
+import { getAllTimezones } from 'countries-and-timezones'
+
+import { getLocales, getCalendars } from 'expo-localization'
 import { I18n } from 'i18n-js'
 
 import en from '@/locales/en.json'
 import fi from '@/locales/fi.json'
 import { ThemedText } from '@/components/themed-text';
-import { initial } from 'lodash';
 
 // Lokalisaatioita
 const i18n = new I18n({
@@ -39,8 +40,8 @@ export interface Settings {
 
 export const initialSettings: Settings = {
     theme: 'default',
-    language: i18n.locale = getLocales()[0].languageCode ?? 'en',
-    timezone: 'EET'
+    language: i18n.locale = getLocales()[0].languageCode ?? 'fi',
+    timezone: getCalendars()[0].timeZone ?? 'Europe/Helsinki'
 }
 
 export const SettingsContext = createContext<Settings>(initialSettings);
@@ -50,8 +51,13 @@ export default function Settings() {
     const [selectedTheme, setSelectedTheme] = useState(initialSettings.theme)    // Tumma/Vaalea/Oletus teemavalikko
     const [currentLanguage, setLanguage] = useState(initialSettings.language)   // Kielivalikko
     const [isSelectTimezoneModalVisible, setSelectTimezoneModalVisible] = useState(false)   // Aikavyöhykevalikko
+    const [selectedTimezone, setSelectedTimezone] = useState(initialSettings.timezone)  // Valittu aikavyöhyke
 
     i18n.locale = settings.language ?? 'en'
+
+    /** Kaikki aikavyöhykkeet listaamista varten */
+    const timezones = Object.values(getAllTimezones());
+    console.log("TZ", timezones)
     
 
     return (<ThemedView>
@@ -87,14 +93,14 @@ export default function Settings() {
                     }
                     style={styles.pickerStyle}
                     >
-                    <Picker.Item label="Oletus" value="default" />
-                    <Picker.Item label="Vaalea" value="light" />
-                    <Picker.Item label="Tumma" value="dark" />
+                    <Picker.Item label={i18n.t('settingsPage.default-theme')} value="default" />
+                    <Picker.Item label={i18n.t('settingsPage.light-theme')} value="light" />
+                    <Picker.Item label={i18n.t('settingsPage.dark-theme')} value="dark" />
                 </Picker>
             </ThemedView>
             <ThemedView style={styles.settingsView}>
                 <ThemedText style={styles.baseText}>{i18n.t('settingsPage.timezone')}</ThemedText>
-                <Button title="Valitse" onPress={() => setSelectTimezoneModalVisible(true)}/>
+                <Button title={i18n.t('settingsPage.select')} onPress={() => setSelectTimezoneModalVisible(true)}/>
             </ThemedView>
         </ThemedView>
         <ThemedView style={styles.settingsViewContainer}>
@@ -117,14 +123,29 @@ export default function Settings() {
 
         {/** Aikavyöhyke modaali */}
         <Modal visible={isSelectTimezoneModalVisible} animationType="fade" transparent={true} onRequestClose={() => setSelectTimezoneModalVisible(false)}>
-            <View style={evStyles.modalBackground}>
-                <View style={evStyles.modalContent}>
-                <Text style={evStyles.modalTitle}>Valitse aikavyöhyke</Text>
-                <Text style={evStyles.modalText}>...aikavyöhykkeitä...</Text>
-                <TouchableOpacity style={evStyles.button} onPress={() => setSelectTimezoneModalVisible(false)}>
-                    <Text style={evStyles.buttonText}>Sulje</Text>
-                </TouchableOpacity>
-                </View>
+            <View style={styles.modalBackground}>
+                <ThemedView style={styles.modalContent}>
+                    <ThemedText style={styles.baseText}>{i18n.t('settingsPage.select-timezone')}</ThemedText>
+                    <ThemedText style={styles.baseText}>Valittu {selectedTimezone}</ThemedText>
+                    <Picker
+                        selectedValue={selectedTimezone}
+                        onValueChange={(itemValue, itemIndex) => {
+                            settings.timezone = itemValue
+                            setSelectedTimezone(itemValue)
+                            }
+                        }
+                        style={styles.pickerStyleModal}
+                        >
+                        {
+                            timezones.map((timezone) => (
+                                <Picker.Item label={`${timezone.name + "   " + timezone.utcOffsetStr}`} value={timezone.name}/>
+                            ))
+                        }
+                    </Picker>
+                    <TouchableOpacity style={evStyles.button} onPress={() => setSelectTimezoneModalVisible(false)}>
+                        <Text style={evStyles.buttonText}>{i18n.t('settingsPage.exit')}</Text>
+                    </TouchableOpacity>
+                </ThemedView>
             </View>
         </Modal>
     </ThemedView>
@@ -174,5 +195,30 @@ const styles = StyleSheet.create({
 
             }
         })        
-    }
+    },
+    pickerStyleModal: {
+        ...Platform.select({
+            android: {
+                height: 75,
+                width: 150,
+                backgroundColor: '#ffffff',
+            },
+            default: {
+                margin: 30,
+                padding: 10
+            }
+        })        
+    },
+    modalBackground: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+    },
+    modalContent: {
+        padding: 20,
+        borderRadius: 10,
+        width: 280,
+        alignItems: "center",
+    },
 })
