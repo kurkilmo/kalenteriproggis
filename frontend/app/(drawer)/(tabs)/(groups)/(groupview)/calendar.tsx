744f6ab3ff2240f206b46eecfd9efe8baa622158
kurkilmo <ilmo.kurki00@gmail.com>
2025-11-27 14:13:56 +0200

    frontend: Use group specific calendar for group calendar view

--- frontend/app/(drawer)/(tabs)/(groups)/(groupview)/calendar.tsx
+++ frontend/app/(drawer)/(tabs)/(groups)/(groupview)/calendar.tsx
@@ -1,6 +1,6 @@
-import { CombinedCalendarView } from '@/components/calendar';
+import { CombinedCalendarView, GroupWeekCalendar } from '@/components/calendar';
 import { ThemedView } from '@/components/themed-view';
-import { getGroupEvents } from '@/services/groups';
+import { getGroupEvents, getGroupExternalBusy } from '@/services/groups';
 import { useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from 'react';
 
@@ -13,16 +13,21 @@ interface Group {
 export default function GroupViewScreen() {
     const { id, name } = useLocalSearchParams();
     const [ events, setEvents ] = useState([])
+    const [ busyTimes, setBusyTimes ] = useState([])
 
     useEffect(() => {
         if (!id) return
         getGroupEvents(
             typeof id === "string" ? id : id[0]
         ).then(setEvents)
+        getGroupExternalBusy(
+            typeof id === "string" ? id : id[0]
+        ).then(setBusyTimes);
     }, [id])
+    console.log(busyTimes)
     return (
         <ThemedView>
-            <CombinedCalendarView events={events} />
+            <GroupWeekCalendar events={events} busy={busyTimes} />
         </ThemedView>
     );
 }
