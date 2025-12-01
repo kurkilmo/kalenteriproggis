f5b1e71fbf548e1899018fa1fe92fbc101767243
kurkilmo <ilmo.kurki00@gmail.com>
2025-12-01 14:18:05 +0200

    frontend: ryhmän etusivulle info ryhmän jäsenistä

--- frontend/app/(drawer)/(tabs)/(groups)/(groupview)/index.tsx
+++ frontend/app/(drawer)/(tabs)/(groups)/(groupview)/index.tsx
@@ -1,26 +1,42 @@
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
+import { getGroupById } from '@/services/groups';
+import styles from "@/styles/groupStyle";
 import { useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from 'react';
+
+interface User {
+    id: number,
+    username: string,
+    displayname: string
+}
 
 interface Group {
     id: number,
     owner_id: number,
-    name: string
+    name: string,
+    users: Array<User>
 }
 
 export default function GroupViewScreen() {
     const { id, name } = useLocalSearchParams();
-    //const [ group, setGroup ] = useState<Group>()
-//
-    //useEffect(() => {
-    //    getGroupById(
-    //        typeof id === "string" ? id : id[0]
-    //    ).then(setGroup)
-    //}, [])
+    const [ group, setGroup ] = useState<Group>()
+
+    useEffect(() => {
+        getGroupById(
+            typeof id === "string" ? id : id[0]
+        ).then(setGroup)
+    }, [id])
 
     return (
-        <ThemedView>
-            <ThemedText>{name}</ThemedText>
+        <ThemedView style={styles.container}>
+            <ThemedText style={styles.title}>{name}</ThemedText>
+            <ThemedText style={{ marginBottom: 20 }}>Jäsenet:</ThemedText>
+            {group?.users.map((user) => {
+                let text = user.displayname
+                if (group.owner_id === user.id) text += " (Omistaja)"
+                return <ThemedText key={user.id}>{text}</ThemedText>
+            })}
         </ThemedView>
     );
 }
